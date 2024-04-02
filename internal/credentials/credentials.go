dd0c6770a37085ce8134c4df040183ee113a5105
bjunker <brian.junker@gmail.com>
2024-04-02 00:29:01 -0700

    Add additional credentials options
    Added the Endpoint, NoVerifySSL and WithProxy options.

--- internal/credentials/credentials.go
+++ internal/credentials/credentials.go
@@ -10,20 +10,72 @@ import (
 )
 
 func CreateCredentials() {
-	actions := githubactions.New()
+	action := githubactions.New()
 
-	privateKeyContent := actions.GetInput("private-key")
-	certificateContent := actions.GetInput("certificate")
-	region := actions.GetInput("aws-region")
-	roleArn := actions.GetInput("role-arn")
-	profileArn := actions.GetInput("profile-arn")
-	trustAnchorArn := actions.GetInput("trust-anchor-arn")
+	privateKeyContent := action.GetInput("private-key")
+	certificateContent := action.GetInput("certificate")
 
+	credentialsOptions := createCredentialsOptions(action)
+
+	privateKey, err := util.GetPrivateKeyFromPEMString([]byte(privateKeyContent))
+	if err != nil {
+		action.Errorf("%s", err)
+		os.Exit(1)
+	}
+	certificate, err := util.GetCertificateFromPEM([]byte(certificateContent))
+	if err != nil {
+		action.Errorf("%s", err)
+		os.Exit(1)
+	}
+	var certificateChain []*x509.Certificate
+
+	signer, signingAlgorithm, err := helper.GetFileSystemSigner(privateKey, certificate, certificateChain)
+	if err != nil {
+		action.Errorf("%s", err)
+		os.Exit(1)
+	}
+	defer signer.Close()
+	credentialProcessOutput, err := helper.GenerateCredentials(&credentialsOptions, signer, signingAlgorithm)
+	if err != nil {
+		action.Errorf("%s", err)
+		os.Exit(1)
+	}
+
+	action.AddMask(credentialProcessOutput.AccessKeyId)
+	action.SetEnv("AWS_ACCESS_KEY_ID", credentialProcessOutput.AccessKeyId)
+	action.SetOutput("aws-access-key-id", credentialProcessOutput.AccessKeyId)
+
+	action.AddMask(credentialProcessOutput.SecretAccessKey)
+	action.SetEnv("AWS_SECRET_ACCESS_KEY", credentialProcessOutput.SecretAccessKey)
+	action.SetOutput("aws-secret-access-key", credentialProcessOutput.SecretAccessKey)
+
+	action.AddMask(credentialProcessOutput.SessionToken)
+	action.SetEnv("AWS_SESSION_TOKEN", credentialProcessOutput.SessionToken)
+	action.SetOutput("aws-session-token", credentialProcessOutput.SessionToken)
+
+	action.SetEnv("AWS_DEFAULT_REGION", credentialsOptions.Region)
+	action.SetEnv("AWS_REGION", credentialsOptions.Region)
+}
+
+func createCredentialsOptions(action *githubactions.Action) helper.CredentialsOpts {
 	sessionDuration := 900
-	sessionDurationOverride := actions.GetInput("session-duration")
+	noVerifySSL := false
+	withProxy := false
+
+	region := action.GetInput("aws-region")
+	roleArn := action.GetInput("role-arn")
+	profileArn := action.GetInput("profile-arn")
+	trustAnchorArn := action.GetInput("trust-anchor-arn")
+	sessionDurationOverride := action.GetInput("session-duration")
+	endpoint := action.GetInput("endpoint")
+	ssl := action.GetInput("no-verify-ssl")
+	proxy := action.GetInput("with-proxy")
+
 	if sessionDurationOverride != "" {
 		sessionDuration, _ = strconv.Atoi(sessionDurationOverride)
 	}
+	noVerifySSL, _ = strconv.ParseBool(ssl)
+	withProxy, _ = strconv.ParseBool(proxy)
 
 	credentialsOptions := helper.CredentialsOpts{
 		RoleArn:           roleArn,
@@ -31,44 +83,10 @@ func CreateCredentials() {
 		TrustAnchorArnStr: trustAnchorArn,
 		SessionDuration:   sessionDuration,
 		Region:            region,
+		Endpoint:          endpoint,
+		NoVerifySSL:       noVerifySSL,
+		WithProxy:         withProxy,
 	}
 
-	privateKey, err := util.GetPrivateKeyFromPEMString([]byte(privateKeyContent))
-	if err != nil {
-		actions.Errorf("%s", err)
-		os.Exit(1)
-	}
-	certificate, err := util.GetCertificateFromPEM([]byte(certificateContent))
-	if err != nil {
-		actions.Errorf("%s", err)
-		os.Exit(1)
-	}
-	var certificateChain []*x509.Certificate
-
-	signer, signingAlgorithm, err := helper.GetFileSystemSigner(privateKey, certificate, certificateChain)
-	if err != nil {
-		actions.Errorf("%s", err)
-		os.Exit(1)
-	}
-	defer signer.Close()
-	credentialProcessOutput, err := helper.GenerateCredentials(&credentialsOptions, signer, signingAlgorithm)
-	if err != nil {
-		actions.Errorf("%s", err)
-		os.Exit(1)
-	}
-
-	actions.AddMask(credentialProcessOutput.AccessKeyId)
-	actions.SetEnv("AWS_ACCESS_KEY_ID", credentialProcessOutput.AccessKeyId)
-	actions.SetOutput("aws-access-key-id", credentialProcessOutput.AccessKeyId)
-
-	actions.AddMask(credentialProcessOutput.SecretAccessKey)
-	actions.SetEnv("AWS_SECRET_ACCESS_KEY", credentialProcessOutput.SecretAccessKey)
-	actions.SetOutput("aws-secret-access-key", credentialProcessOutput.SecretAccessKey)
-
-	actions.AddMask(credentialProcessOutput.SessionToken)
-	actions.SetEnv("AWS_SESSION_TOKEN", credentialProcessOutput.SessionToken)
-	actions.SetOutput("aws-session-token", credentialProcessOutput.SessionToken)
-
-	actions.SetEnv("AWS_DEFAULT_REGION", region)
-	actions.SetEnv("AWS_REGION", region)
+	return credentialsOptions
 }
